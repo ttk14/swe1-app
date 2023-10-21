48927f66f9a873c919697dc5c4caa496ddd3178c
Kaleab <132639842+ttk14@users.noreply.github.com>
2023-10-21 15:09:32 -0500

    Update test_views.py
    update test_views

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,7 +1,7 @@
-
 from django.test import TestCase
 from django.urls import reverse
-class TestViews(TestCase):
 
+
+class TestViews(TestCase):
     def test_should_show_index_page(self):
-        self.assertEqual(0,0)
+        self.assertEqual(0, 0)
